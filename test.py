e33c0e5573b67b33b8be81ebb9481615152ce8dc
Andrew Landau <andrewtylerlandau@gmail.com>
2024-06-04 16:05:22 +0100

    testing code

--- test.py
+++ test.py
@@ -1,49 +1,11 @@
-from ptrseq.utils import ConditionalArgumentParser
-from ptrseq.utils import argbool
+from conditional_parser import ConditionalArgumentParser
+from ptrseq.utils import argbool, argintrange
 
 
 def main():
     parser = ConditionalArgumentParser(description="Task-based argument parser example.")
-    parser.add_argument("task", type=str, help="The task to run.")
-    parser.add_argument("--use_curriculum", type=argbool, default=False)
-    parser.add_conditional(
-        "task",
-        "dominoe",
-        "--learning_mode",
-        type=str,
-        default="supervised",
-        help="The learning mode to use.",
-    )
-    parser.add_conditional(
-        "task",
-        "dominoe",
-        "--randomize_direction",
-        type=str,
-        default="yes",
-        help="whether to use random dominoe directions.",
-    )
-
-    parser.add_conditional(
-        "use_curriculum",
-        True,
-        "--curriculum",
-        default="standard",
-        type=str,
-        help="Which curriculum to use",
-    )
-
-    parser.add_conditional(
-        "curriculum",
-        "nonstandard",
-        "--curriculum_path",
-        default="",
-        type=str,
-        help="The path to the curriculum file.",
-    )
-
-    # Parse the arguments
-    # args = ["dominoe", "--use_curriculum", "True", "--curriculum", "nonstandard", "--help"]
-    args = parser.parse_args(args=None)
+    parser.add_argument("--none-or-2", default=None, nargs=2, type=argintrange)
+    args = parser.parse_args()
 
     print(vars(args))
 
